7bb3d17096149a71db044d00164b2a2cf3f7ec7e
Ashok <134892274+ashok5141@users.noreply.github.com>
2025-05-29 08:37:39 -0500

    Update ec2.tf
    Temporary fix

--- cloudgoat/scenarios/aws/beanstalk_secrets/terraform/ec2.tf
+++ cloudgoat/scenarios/aws/beanstalk_secrets/terraform/ec2.tf
@@ -4,7 +4,7 @@
 
 data "aws_elastic_beanstalk_solution_stack" "python_stack" {
   most_recent = true # Get the latest version matching the regex
-  name_regex  = "^64bit Amazon Linux 2023.*running Python 3.11$"
+  name_regex  = "64bit Amazon Linux 2023 v4.5.2 running Python 3.13"
 }
 
 
